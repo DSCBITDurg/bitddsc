684c5b8c9a287a9b8114b054350891251a33f274
Abhishek Agrawal <abhishekagrawal8888@gmail.com>
2020-09-12 23:06:42 +0530

    fixed card width and hover effect on navlink

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,11 +5,13 @@ import React from 'react';
 // import css file
 import styles from './HomePage.module.css';
 import dscHomeImage from '../assets/images/dsc-home.svg' 
+import HomeSection from '../components/HomeSection/HomeSection';
 function HomePage() {
     return (
         <div className={styles.container}>
             <div className={styles.image}>
-                <img src={dscHomeImage}/>
+              <HomeSection leftOrientIamge={false}/>
+              
             </div>
         </div>
     );
