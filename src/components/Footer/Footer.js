a03f8ad67bdc3be5725ec717b41e087c5bb07f18
rohitgeddam <rohitgeddam2018@gmail.com>
2020-09-12 23:50:21 +0530

    modified footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,13 @@ import React from 'react';
 
 //import images
 import dscIcon from '../../assets/images/dscIcon.png';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 
+import discordIcon from '../../assets/images/discord.svg'
+import instagramIcon from '../../assets/images/instagram.svg'
+import linkedinIcon from '../../assets/images/linkedin.svg'
+import twitterIcon from '../../assets/images/twitter.svg'
+import youtubeIcon from '../../assets/images/youtube.svg'
+import whatsappIcon from '../../assets/images/whatsapp.svg' 
 //import styles
 import styles from './Footer.module.css';
 
@@ -17,41 +21,19 @@ export default function Footer(){
             
             <div className={styles.footerMain}>
              
-                <img src={dscIcon} className={styles.dscIcon} alt="Loading..."/>
-                 
-             
-
-                <div className={styles.links}>
-                    <a className={styles.link} href="/">Home</a>
-                    <a className={styles.link} href="/">About</a>
-                    <a className={styles.link} href="/">Blog</a>
-                    <a className={styles.link} href="/">Contact</a>
-                    <a className={styles.link} href="/">Events</a>
-                </div>
-
-                <div className={styles.features}>
-                    <a className={styles.link} href="/">Collab Tool</a>
-                    <a className={styles.link} href="/">My Badges</a>
-                </div>
-
-                <div className={styles.eventsAndUs}>
-                    <a className={styles.link} href="/">Our Team</a>
-                    <a className={styles.link} href="/">Upcomming Events</a>
-                    <a className={styles.link} href="/">Past Events</a>
-                    <a className={styles.link} href="/">Gallary</a>
-                </div>
+                    <img src={dscIcon} className={styles.dscIcon} alt="Loading..."/>
+              
+                    <div className={styles.joinus}>
+                        <a target="_blank" href="https://chat.whatsapp.com/EYEEcsfpUPV9C5MKjNj7QY"><img src={whatsappIcon}/></a>
+                        <a target="_blank" href="https://www.instagram.com/bitddsc/"><img src={instagramIcon}/></a>
+                        <a target="_blank" href="https://www.linkedin.com/company/dsc-bitd/"><img src={linkedinIcon}/></a>
+                        <a target="_blank" href="https://twitter.com/BitdDsc"><img src={twitterIcon}/></a>
+                        <a target="_blank" href="https://www.youtube.com/channel/UCT6bau44YmuFM3Qu_de6W_w" ><img src={youtubeIcon}/></a>
+                        <a target="_blank" href="https://discord.gg/DjgrJUP"><img src={discordIcon}/></a>
+                    </div>
+                   
             </div>
-
-            <div className={styles.socialHandles}>
-                <div className={styles.socialIcons}>
-                    <p>Social Handles</p>
-                    <p><InstagramIcon fontSize="large"/></p>
-                    <p><WhatsAppIcon fontSize="large"/></p>
-                </div>
-                <div className={styles.email}>
-                    <p>Email: bitdsc@gmail.com</p>
-                </div>
-            </div>
+           
         </div>
     );
 }
