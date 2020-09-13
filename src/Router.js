490f84f0c38dfb58fc22ddc55ae14aab7bb835d4
rohitgeddam <rohitgeddam2018@gmail.com>
2020-09-13 22:17:53 +0530

    add default route in router

--- src/Router.js
+++ src/Router.js
@@ -13,9 +13,9 @@ import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import EventsPage from './pages/EventsPage';
 
-import CollabPage from './pages/CollabPage';
-import BadgePage from './pages/BadgePage';
-import UserPage from './pages/UserPage';
+// import CollabPage from './pages/CollabPage';
+// import BadgePage from './pages/BadgePage';
+// import UserPage from './pages/UserPage';
 
 
 import Layout from './components/Layout/Layout';
@@ -33,7 +33,7 @@ export default function Router() {
 
                     <Route  path={`${githubUrlPage}/contact`} component={ContactPage}/>
                     <Route  path={`${githubUrlPage}/events`} component={EventsPage}/>
-
+                    <Route component={HomePage}/>
                     {/* <Route  path='/collab' component={CollabPage}/>
                     <Route  path='/badges' component={BadgePage}/>
                     <Route  path='/user' component={UserPage}/> */}
